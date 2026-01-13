25d8bf71194e0b84ba67a3b97e04f5b7d319c953
Ippei Obayashi <ohai@kmc.gr.jp>
2026-01-13 14:46:23 +0900

    benchmark: update benchmark code

--- benchmark/phat_mphhc_benchmark.py
+++ benchmark/phat_mphhc_benchmark.py
@@ -34,37 +34,40 @@ def alpha_filtration_from_uniform_random_points(rng, n):
 
 def benchmark_alpha_filtration(repeat, num_points):
     rng = np.random.default_rng(58340232)
-    ptimes_1 = []
-    ptimes_2 = []
-    ptimes_3 = []
+    times = [[], [], [], []]
     for _ in range(repeat):
         index_to_simplex, simplex_to_index = (
             alpha_filtration_from_uniform_random_points(rng, num_points)
         )
-        m1 = phat.Matrix(len(index_to_simplex), "none")
-        m2 = mphhc.Matrix(3)
-        m3 = mphhc.Matrix(3)
+        matrices = [
+            phat.Matrix(len(index_to_simplex), "none"),
+            mphhc.Matrix(3),
+            mphhc.Matrix(3),
+            mphhc.Matrix(3, True),
+        ]
         for index, simplex in enumerate(index_to_simplex):
             dim = len(simplex) - 1
             b = [simplex_to_index[t] for t in boundary_of_simplex(simplex)]
-            m1.set_dim_col(index, dim, b)
-            m2.set_dim_col(index, dim, b)
-            m3.set_dim_col(index, dim, b)
+            for m in matrices:
+                m.set_dim_col(index, dim, b)
 
-        ptimes_1.append(run(m1.reduce_twist))
-        ptimes_2.append(run(m2.reduce_twist))
-        ptimes_3.append(run(m3.reduce_standard))
+        times[0].append(run(matrices[0].reduce_twist))
+        times[1].append(run(matrices[1].reduce_twist))
+        times[2].append(run(matrices[2].reduce_standard))
+        times[3].append(run(matrices[3].reduce_standard))
         # print(set(m1.birth_death_pairs()) ^ set(m2.birth_death_pairs()))
         # print(set(m1.birth_death_pairs()) ^ set(m3.birth_death_pairs()))
         # assert set(m1.birth_death_pairs()) == set(m2.birth_death_pairs())
 
     print(
         "phat:",
-        np.mean(ptimes_1),
+        np.mean(times[0]),
         "mphhc-twist:",
-        np.mean(ptimes_2),
+        np.mean(times[1]),
         "mphhc-standard:",
-        np.mean(ptimes_3),
+        np.mean(times[2]),
+        "mphhc-standard-with-basis:",
+        np.mean(times[3]),
     )
 
 
@@ -97,6 +100,6 @@ def benchmark_random_3d_cubical(repeat, size):
 benchmark_alpha_filtration(repeat=5, num_points=10000)
 # phat: 0.021183716505765914 mphhc-twist: 0.012243387848138809 mphhc-standard: 0.1491922608576715
 # benchmark_alpha_filtration(repeat=5, num_points=100000)
-benchmark_random_3d_cubical(repeat=5, size=(50, 50, 50))
+# benchmark_random_3d_cubical(repeat=5, size=(50, 50, 50))
 # phat: 0.13374500460922717 mphhc: 0.09117477238178254
 # benchmark_random_3d_cubical(repeat=5, size=(100, 100, 100))
